81ed3c5986017c1bab69e04d414fd0509857978a
Alejandro <100007465.joan23@fje.edu>
2022-04-25 16:48:52 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function calcProp() {
+    var total = document.getElementById('total').value
+    var servicio = document.getElementById('servicio'.value)
+    var personas = document.getElementById('peronas').value
+}
